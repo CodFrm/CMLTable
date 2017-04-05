8f43c5f193056cd541b89037a3fe3633d4998e4b
CodFrm <code.farmer@qq.com>
2017-04-05 16:12:13 +0800

    添加了几个函数,233

--- cmltable/cmltable.js
+++ cmltable/cmltable.js
@@ -2,50 +2,37 @@
     var createCMLTable = function(tableJson) {
         var TableObject;
         var tableHtml = '<table class="cmltable">\n<tr>\n';
-        var selectHtml='<select class="cmltable-select">\n';
+        var selectHtml = '<select class="cmltable-select">\n';
         var columns = tableJson.columns;
         for (var i = 0; i < columns.length; i++) {
             tableHtml += '<th>\n';
             if (columns[i].checkbox == true) {
-                tableHtml += '<div class="cmltable-check cmltable-check-head" id="cmltable-check-' + i + '"></div>\n';
+                tableHtml += '<div class="cmltable-check cmltable-check-head"></div>\n';
             } else if (columns[i].field) {
-                tableHtml += columns[i].title+'\n';
+                tableHtml += columns[i].title + '\n';
             }
             tableHtml += '</th>\n';
         }
-        for(var i=0;i<tableJson.action.length;i++){
-        	selectHtml+=' <option value="'+tableJson.action[i].action+'">'+tableJson.action[i].title+'</option>\n';
+        for (var i = 0; i < tableJson.action.length; i++) {
+            selectHtml += ' <option value="' + tableJson.action[i].action + '">' + tableJson.action[i].title + '</option>\n';
         }
-        jQuery.ajax({
-            url: tableJson.url,
-            success: function(data) {
-                var jsonData = $.parseJSON(data);
-                for (var n = 0; n < jsonData.rows.length; n++) {
-                    var newRow = "<tr>";
-                    for (var i = 0; i < columns.length; i++) {
-                        newRow += "<td>";
-                        if (columns[i].checkbox == true) {
-                            newRow += '<div class="cmltable-check cmltable-check-' + i + '"></div>\n';
-                        } else if (columns[i].field) {
-                            newRow += jsonData.rows[n][columns[i].field]+'\n';
-                        }
-                        newRow += "</td>\n";
-                    }
-                    newRow += "</tr>\n"
-                    TableObject.find('tr:last').after(newRow);
+
+        tableHtml += '</tr>\n</table>\n'
+        selectHtml += '</select>\n';
+        $(this).html(selectHtml + '<button class="cmltable-btn cmltable-sublit">确定</button>\n' + '<div class="cmltable-search">\n<input type="text" class="cmltable-select cmltable-search-edit" />\n' + '\n<button class="cmltable-btn  cmltable-search-btn">搜索</button>\n</div>' + tableHtml);
+        $(this).find('.cmltable-sublit').click(function(event) { //选择列表框确定
+            for (var i = 0; i < tableJson.action.length; i++) {
+                if (tableJson.action[i].action == $(this).prev().val()) {
+                    tableJson.action[i].func(TableObject);
+                    break;
                 }
             }
         });
-        tableHtml += '</tr>\n</table>\n'
-        selectHtml+='</select>\n';
-        $(this).html(selectHtml+'<button class="cmltable-btn cmltable-sublit">确定</button>\n' + '<div class="cmltable-search">\n<input type="text" class="cmltable-select cmltable-search-edit" />\n' + '\n<button class="cmltable-btn  cmltable-search-btn">搜索</button>\n</div>' + tableHtml);
-        $(this).find('.cmltable-sublit').click(function(event) {
-
-        });
-        $(this).find('.cmltable-check-head').click(function(event) {
-            headCheck = $(this);
-            TableObject.find('.' + $(this).attr('id')).each(function(index, el) {
-                if (headCheck.attr('check') == 'true') {
+        $(this).find('.cmltable-check-head').click(function(event) { //全选
+            headCheck = $(this).attr('check');
+            TableObject.find('.cmltable-check').each(function(index, el) {
+                if (index == 0) return 0;
+                if (headCheck == 'true') {
                     $(this).text('');
                     $(this).attr('check', 'false');
                 } else {
@@ -55,23 +42,65 @@
             });
 
         });
+
         TableObject = $(this).children('.cmltable');
+        /**
+         * 获取选中
+         * @return {json} [选中项]
+         */
+        TableObject.getSelect = function() {
+            var check = {};
+            var i = 0;
+            $(this).find('.cmltable-check').each(function(index, el) {
+                if ($(this).attr('check') == 'true') {
+                    check['id[' + i++ + ']'] = $(this).attr('cml-value');
+                }
+            });
+            return check;
+        };
+        /**
+         * 刷新
+         * @return {null}
+         */
+        TableObject.refresh = function() {
+            TableObject.find("tr:not(:first)").remove();
+            jQuery.ajax({ //读取内容
+                url: tableJson.url,
+                success: function(data) {
+                    var jsonData = $.parseJSON(data);
+                    for (var n = 0; n < jsonData.rows.length; n++) {
+                        var newRow = "<tr>";
+                        for (var i = 0; i < columns.length; i++) {
+                            newRow += "<td>";
+                            if (columns[i].checkbox == true) {
+                                newRow += '<div class="cmltable-check" cml-value="' + jsonData.rows[n][tableJson.keyID] + '"></div>\n';
+                            } else if (columns[i].field) {
+                                newRow += jsonData.rows[n][columns[i].field] + '\n';
+                            }
+                            newRow += "</td>\n";
+                        }
+                        newRow += "</tr>\n"
+                        TableObject.find('tr:last').after(newRow);
+                    }
+                }
+            });
+        };
+        TableObject.refresh();
         return TableObject;
     };
-
     $.fn.createCMLTable = createCMLTable;
 })(jQuery);
 
 
+
 jQuery(document).ready(function($) {
-    $('.cmltable-check').click(function(event) {
+    $(document).on( 'click','.cmltable-check',function(event) {
         if ($(this).attr('check') == 'true') {
             $(this).text('');
             $(this).attr('check', 'false');
         } else {
             $(this).text('✔');
             $(this).attr('check', 'true');
-
         }
     });
 });
